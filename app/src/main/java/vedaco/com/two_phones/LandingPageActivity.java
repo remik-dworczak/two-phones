281c174ceafa4b9c712eedea9694673d71cf7b1a
chsdkr@audatex.com <frank.hoyler@solera.com>
2019-01-06 18:34:32 +0100

    Skeleton code

--- app/src/main/java/vedaco/com/two_phones/LandingPageActivity.java
+++ app/src/main/java/vedaco/com/two_phones/LandingPageActivity.java
@@ -3,7 +3,7 @@ package vedaco.com.two_phones;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LandingPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
